8205ff09a7171c4fe4ce08cddfb1f5ef2d5ecff0
ByeongHyeon <qudgus1984@naver.com>
2022-05-30 16:03:49 +0900

    프로토콜 확장 (기본값 설정)

--- Allen study/main.swift
+++ Allen study/main.swift
@@ -1599,45 +1599,71 @@ import Foundation
 
 
 // 프로토콜 상속
+//protocol Remote {
+//    func turnOn()
+//    func turnOff()
+//}
+//
+//protocol AirConRemote {
+//    func Up()
+//    func Down()
+//}
+//
+//protocol SuperRemoteProtocol: Remote, AirConRemote {
+//    // 프로토콜끼리 상속 구조를 만드는 것이 가능
+//    func doSomething()
+//}
+//
+//class HomePot: SuperRemoteProtocol {
+//    func turnOn() {
+//        print("킨다")
+//    }
+//    func turnOff() {
+//        print("끈다")
+//    }
+//    func Up() {
+//        print("올린다")
+//    }
+//    func Down() {
+//        print("내린다")
+//    }
+//    func doSomething() {
+//        print("그이외")
+//    }
+//}
+//
+//// 선택적 요구사항
+//
+//// 어트리뷰트
+//// @available, @objc, @IBOutlet, @IBAction 등등
+//
+//// 실제 예시
+//@available(iOS 13.0, macOS 10.12, *) // 선언에 추가정보 제공, 타입에 추가정보 제공
+//class SomeType {
+//}
+
+// 프로토콜 확장
+
 protocol Remote {
     func turnOn()
     func turnOff()
 }
 
-protocol AirConRemote {
-    func Up()
-    func Down()
-}
-
-protocol SuperRemoteProtocol: Remote, AirConRemote {
-    // 프로토콜끼리 상속 구조를 만드는 것이 가능
-    func doSomething()
-}
-
-class HomePot: SuperRemoteProtocol {
-    func turnOn() {
-        print("킨다")
-    }
-    func turnOff() {
-        print("끈다")
-    }
-    func Up() {
-        print("올린다")
-    }
-    func Down() {
-        print("내린다")
-    }
-    func doSomething() {
-        print("그이외")
+extension Remote {
+    func turnOn() { print("리모콘 켜기")}
+    func turnOff() {print("리모콘 끄기")}
+    
+    func doAnotherAction() {
+        print("다른 동작 수행")
     }
 }
 
-// 선택적 요구사항
-
-// 어트리뷰트
-// @available, @objc, @IBOutlet, @IBAction 등등
-
-// 실제 예시
-@available(iOS 13.0, macOS 10.12, *) // 선언에 추가정보 제공, 타입에 추가정보 제공
-class SomeType {
+class TV1: Remote {
+//    func turnOn() { print("리모콘 켜기")}
+//    func turnOff() {print("리모콘 끄기")}
 }
+var tv7 = TV1()
+tv7.turnOn()
+tv7.turnOff()
+tv7.doAnotherAction()   
+
