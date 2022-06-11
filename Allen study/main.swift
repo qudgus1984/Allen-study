06e53f5b615d483bf701a4c1f72787ac2a786438
ByeongHyeon <qudgus1984@naver.com>
2022-06-12 00:26:17 +0900

    뷰 컨트롤러 메모리 해제

--- Allen study/main.swift
+++ Allen study/main.swift
@@ -2348,35 +2348,35 @@ import Foundation
 // 메모리 누수의 사례
 // 강한참조 사이클로 인한 메모리누수의 사례
 
-class Dog {
-    var name = "초코"
-    
-    var run: (() -> Void)?
-    
-    func walk() {
-        print("\(self.name)가 걷는다.")
-    }
-    
-    func saveClosure() {
-        // 클로저를 인스턴스의 변수에 저장
-        run = {
-            print("\(self.name)가 뛴다.")
-        }
-        
-    }
-    
-    deinit {
-        print("\(self.name) 메모리 해제")
-    }
-}
-
-func doSomething() {
-    let choco: Dog? = Dog()
+//class Dog {
+//    var name = "초코"
+//
+//    var run: (() -> Void)?
+//
+//    func walk() {
+//        print("\(self.name)가 걷는다.")
+//    }
+//
+//    func saveClosure() {
+//        // 클로저를 인스턴스의 변수에 저장
+//        run = {
+//            print("\(self.name)가 뛴다.")
+//        }
+//
+//    }
+//
+//    deinit {
+//        print("\(self.name) 메모리 해제")
+//    }
+//}
+//
+//func doSomething() {
+//    let choco: Dog? = Dog()
     //choco?.saveClosure() // 강한 참조사이클 일어남 (메모리 누수가 일어남)
-}
-
-print(doSomething())
-doSomething()
+//}
+//
+//print(doSomething())
+//doSomething()
 
 class ViewController: UIViewController {
     var name: String = "뷰컨"
