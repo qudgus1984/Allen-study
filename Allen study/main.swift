c786b47d5277b6fdd60665b7c7eb85c8989c0352
ByeongHyeon <qudgus1984@naver.com>
2022-06-15 14:17:21 +0900

    part 1-6 복습 완료

--- Allen study/main.swift
+++ Allen study/main.swift
@@ -2714,166 +2714,246 @@ import Foundation
 //}
 //
 // 서버에서 주는 데이터 ===========================================================
-struct MovieData: Codable {
-    let boxOfficeResult: BoxOfficeResult
-}
-
-// MARK: - BoxOfficeResult
-struct BoxOfficeResult: Codable {
-    let dailyBoxOfficeList: [DailyBoxOfficeList]
-}
-
-// MARK: - DailyBoxOfficeList
-struct DailyBoxOfficeList: Codable {
-    let rank: String
-    let movieNm: String
-    let audiCnt: String
-    let audiAcc: String
-    let openDt: String
-}
-
-
-
-// 내가 만들고 싶은 데이터 (우리가 쓰려는 Struct / Class) =======================
-struct Movie {
-    static var movieId: Int = 0   // 아이디가 하나씩 부여되도록 만듦
-    let movieName: String
-    let rank: Int
-    let openDate: String
-    let todayAudience: Int
-    let totalAudience: Int
-    
-    init(movieNm: String, rank: String, openDate: String, audiCnt: String, accAudi: String) {
-        self.movieName = movieNm
-        self.rank = Int(rank)!
-        self.openDate = openDate
-        self.todayAudience = Int(audiCnt)!
-        self.totalAudience = Int(accAudi)!
-        Movie.movieId += 1
-    }
-    
-}
-
-
-
-// 서버와 통신 ===========================================================
-struct MovieDataManager {
-    
-    let movieURL = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?"
-    
-    let myKey = "ed5188d191b44830a2133865842b6868"
-    
-    func fetchMovie(date: String, completion: @escaping ([Movie]?) -> Void) {
-        let urlString = "\(movieURL)&key=\(myKey)&targetDt=\(date)"
-        performRequest(with: urlString) { movies in
-            completion(movies)
-        }
-    }
-    
-    func performRequest(with urlString: String, completion: @escaping ([Movie]?) -> Void) {
-        print(#function)
-        
-        // 1. URL 구조체 만들기
-        guard let url = URL(string: urlString) else { return }
-        
-        // 2. URLSession 만들기 (네트워킹을 하는 객체 - 브라우저 같은 역할)
-        let session = URLSession(configuration: .default)
-        
-        // 3. 세션에 작업 부여
-        let task = session.dataTask(with: url) { (data, response, error) in
-            if error != nil {
-                print(error!)
-                completion(nil)
-                return
-            }
-            
-            guard let safeData = data else {
-                completion(nil)
-                return
-            }
-            
-            
-            // 데이터 분석하기
-            if let movies = self.parseJSON(safeData) {
-                //print("parse")
-                completion(movies)
-            } else {
-                completion(nil)
-            }
-        }
-        
-        // 4.Start the task
-        task.resume()   // 일시정지된 상태로 작업이 시작하기 때문
-    }
-    
-    
-    func parseJSON(_ movieData: Data) -> [Movie]? {
-        // 함수실행 확인 코드
-        print(#function)
-        
-        let decoder = JSONDecoder()
-        
-        do {
-            let decodedData = try decoder.decode(MovieData.self, from: movieData)
-            
-            let dailyLists = decodedData.boxOfficeResult.dailyBoxOfficeList
-            
-            // 반복문으로 movie배열 생성 ⭐️
-//            var myMovielists = [Movie]()
+//struct MovieData: Codable {
+//    let boxOfficeResult: BoxOfficeResult
+//}
 //
-//            for movie in dailyLists {
+//// MARK: - BoxOfficeResult
+//struct BoxOfficeResult: Codable {
+//    let dailyBoxOfficeList: [DailyBoxOfficeList]
+//}
 //
-//                let name = movie.movieNm
-//                let rank = movie.rank
-//                let openDate = movie.openDt
-//                let todayAudi = movie.audiCnt
-//                let accAudi = movie.audiAcc
+//// MARK: - DailyBoxOfficeList
+//struct DailyBoxOfficeList: Codable {
+//    let rank: String
+//    let movieNm: String
+//    let audiCnt: String
+//    let audiAcc: String
+//    let openDt: String
+//}
 //
-//                let myMovie = Movie(movieNm: name, rank: rank, openDate: openDate, audiCnt: todayAudi, accAudi: accAudi)
 //
-//                myMovielists.append(myMovie)
+//
+//// 내가 만들고 싶은 데이터 (우리가 쓰려는 Struct / Class) =======================
+//struct Movie {
+//    static var movieId: Int = 0   // 아이디가 하나씩 부여되도록 만듦
+//    let movieName: String
+//    let rank: Int
+//    let openDate: String
+//    let todayAudience: Int
+//    let totalAudience: Int
+//
+//    init(movieNm: String, rank: String, openDate: String, audiCnt: String, accAudi: String) {
+//        self.movieName = movieNm
+//        self.rank = Int(rank)!
+//        self.openDate = openDate
+//        self.todayAudience = Int(audiCnt)!
+//        self.totalAudience = Int(accAudi)!
+//        Movie.movieId += 1
+//    }
+//
+//}
+//
+//
+//
+//// 서버와 통신 ===========================================================
+//struct MovieDataManager {
+//
+//    let movieURL = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?"
+//
+//    let myKey = "ed5188d191b44830a2133865842b6868"
+//
+//    func fetchMovie(date: String, completion: @escaping ([Movie]?) -> Void) {
+//        let urlString = "\(movieURL)&key=\(myKey)&targetDt=\(date)"
+//        performRequest(with: urlString) { movies in
+//            completion(movies)
+//        }
+//    }
+//
+//    func performRequest(with urlString: String, completion: @escaping ([Movie]?) -> Void) {
+//        print(#function)
+//
+//        // 1. URL 구조체 만들기
+//        guard let url = URL(string: urlString) else { return }
+//
+//        // 2. URLSession 만들기 (네트워킹을 하는 객체 - 브라우저 같은 역할)
+//        let session = URLSession(configuration: .default)
+//
+//        // 3. 세션에 작업 부여
+//        let task = session.dataTask(with: url) { (data, response, error) in
+//            if error != nil {
+//                print(error!)
+//                completion(nil)
+//                return
 //            }
-            
-            // 고차함수를 이용해 movie배열 생성하는 경우 ⭐️
-            let myMovielists = dailyLists.map {
-                Movie(movieNm: $0.movieNm, rank: $0.rank, openDate: $0.openDt, audiCnt: $0.audiCnt, accAudi: $0.audiAcc)
-            }
-            
-            return myMovielists
-            
-        } catch {
-            //print(error.localizedDescription)
-            
-            // (파싱 실패 에러)
-            print("파싱 실패")
-            
-            return nil
-        }
-        
-    }
-    
+//
+//            guard let safeData = data else {
+//                completion(nil)
+//                return
+//            }
+//
+//
+//            // 데이터 분석하기
+//            if let movies = self.parseJSON(safeData) {
+//                //print("parse")
+//                completion(movies)
+//            } else {
+//                completion(nil)
+//            }
+//        }
+//
+//        // 4.Start the task
+//        task.resume()   // 일시정지된 상태로 작업이 시작하기 때문
+//    }
+//
+//
+//    func parseJSON(_ movieData: Data) -> [Movie]? {
+//        // 함수실행 확인 코드
+//        print(#function)
+//
+//        let decoder = JSONDecoder()
+//
+//        do {
+//            let decodedData = try decoder.decode(MovieData.self, from: movieData)
+//
+//            let dailyLists = decodedData.boxOfficeResult.dailyBoxOfficeList
+//
+//            // 반복문으로 movie배열 생성 ⭐️
+////            var myMovielists = [Movie]()
+////
+////            for movie in dailyLists {
+////
+////                let name = movie.movieNm
+////                let rank = movie.rank
+////                let openDate = movie.openDt
+////                let todayAudi = movie.audiCnt
+////                let accAudi = movie.audiAcc
+////
+////                let myMovie = Movie(movieNm: name, rank: rank, openDate: openDate, audiCnt: todayAudi, accAudi: accAudi)
+////
+////                myMovielists.append(myMovie)
+////            }
+//
+//            // 고차함수를 이용해 movie배열 생성하는 경우 ⭐️
+//            let myMovielists = dailyLists.map {
+//                Movie(movieNm: $0.movieNm, rank: $0.rank, openDate: $0.openDt, audiCnt: $0.audiCnt, accAudi: $0.audiAcc)
+//            }
+//
+//            return myMovielists
+//
+//        } catch {
+//            //print(error.localizedDescription)
+//
+//            // (파싱 실패 에러)
+//            print("파싱 실패")
+//
+//            return nil
+//        }
+//
+//    }
+//
+//}
+//
+//
+//// 뷰컨트롤러에서 일어나는 일 ===========================================================
+//// 빈배열
+//var downloadedMovies = [Movie]()
+//
+//// 데이터를 다운로드 및 분석/변환하는 구조체
+//let movieManager = MovieDataManager()
+//
+//
+//// 실제 다운로드 코드
+//movieManager.fetchMovie(date: "20210201") { (movies) in
+//
+//    if let movies = movies {
+//
+//        // 배열 받아서 빈배열에 넣기
+//        downloadedMovies = movies
+//        dump(downloadedMovies)
+//
+//        print("전체 영화 갯수 확인: \(Movie.movieId)")
+//    } else {
+//        print("영화데이터가 없습니다. 또는 다운로드에 실패했습니다.")
+//    }
+//}
+
+// switch 문의 응용
+var num = 12
+switch num {
+case let x where x % 2 == 0: // where 절을 사용해 조건을 붙이는 것.
+    print("짝수 숫자: \(x)")
+default:
+    print("홀수 숫자: \(num)")
 }
 
-
-// 뷰컨트롤러에서 일어나는 일 ===========================================================
-// 빈배열
-var downloadedMovies = [Movie]()
-
-// 데이터를 다운로드 및 분석/변환하는 구조체
-let movieManager = MovieDataManager()
-
-
-// 실제 다운로드 코드
-movieManager.fetchMovie(date: "20210201") { (movies) in
-    
-    if let movies = movies {
-        
-        // 배열 받아서 빈배열에 넣기
-        downloadedMovies = movies
-        dump(downloadedMovies)
-        
-        print("전체 영화 갯수 확인: \(Movie.movieId)")
-    } else {
-        print("영화데이터가 없습니다. 또는 다운로드에 실패했습니다.")
-    }
+// 와일드카드 패턴 : 생략을 위해 사용하는 (_)언더바
+for _ in 1...5 {
+    print("5번 반복")
 }
+
+//사용 예시
+for num in (1...10).reversed() {
+    print(num)
+}
+
+for item in stride(from: 1, to: 15, by: 2) {
+    print(item)
+}
+
+// continue
+for num in 1...30 {
+    if num % 2 == 0 {
+        continue
+    }
+    print(num)
+}
+
+// break
+for num in 1...20 {
+    if num % 2 == 0 {
+        break
+    }
+    print(num)
+}
+
+//tuple 예시
+let IntroduceMySelf = ("Cody", 25, "서울시")
+let IntroduceMySelf1 = (name: "Cody", age:25, adress:"서울시")
+
+//튜플의 내부의 각각 데이터 접근
+IntroduceMySelf.0 // "Cody"
+IntroduceMySelf.1 // 25
+
+// 튜플의 각 요소를 상수화, 변수화 가능(바인딩)
+let (name, age, adress) = ("Cody", 25, "서울시")
+print(name) // "Cody"
+print(adress) // "서울시"
+
+//튜플은 switch문과 자주 활용
+let iOS = ("Swift","5")
+
+switch iOS {
+case("Swift","5"):
+    print("스위프트 버전 5입니다.")
+case ("Swift","4"):
+    print("스위프트 버전 4입니다.")
+default:
+    break
+}
+
+// 이렇게도 사용 가능
+if iOS == ("Swift","5") {
+    print("스위프트 버전 5입니다.")
+}
+
+// 튜플을 사용하는 이유 : 함수는 원칙적으로 리턴값이 한개만 존재하기 때문에 여러개의 값을 변환할 수 없지만,
+// 어떤 묶음 값으로 변환하는 것은 가능
+
+func Example(name: String) -> (Int, String) {
+    
+    return (7, name)
+}
+print(Example(name: "Cody"))
+//이런 식으로 활용 가능하다는 의미!
+
